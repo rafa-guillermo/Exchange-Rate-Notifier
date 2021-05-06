983e4849c6adbbe43bcb387d137125fd05d829e3
Rafael Guillermo <rafael@garcia-irvine.com>
2021-05-06 20:33:00 +0200

    Create consts.js

---
+++ consts.js
@@ -0,0 +1,9 @@
+const endpoint = 'http://data.fixer.io/api/latest'
+const apiKey = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx' // Fixer.io API key
+
+// TWILIO SETTINGS
+// Available from the Twilio dashboard
+const twilioAccSID = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+const twilioAuthToken = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+const twilioNumber = "+00000000000"
+const baseUrl = "https://api.twilio.com/2010-04-01"
